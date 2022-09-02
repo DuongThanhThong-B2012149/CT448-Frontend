1c43920ecc0c5ae146519f46ccef40fc08f36052
thanhthong <thongduong61134@gmail.com>
2022-09-02 11:32:57 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,20 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "../views/ContactBook.vue";
+
 import NotFound from "../views/NotFound.vue";
+import ContactEdit from "../views/ContactEdit.vue";
 
 const routes = [
   {
     path: "/",
     name: "contactbook",
     component: ContactBook,
+  },
+  {
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: ContactEdit,
+    props: true, // Truyền các biến trong $route.params vào làm props
   },
   {
     path: "/:pathMatch(.*)*",
